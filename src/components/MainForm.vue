e33e89370d394b9cff74849313d72998da263b2e
lonshakova <lonshakova.ae@edu.spbstu.ru>
2024-08-15 09:55:44 +0300

    123

---
+++ src/components/MainForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <form class="main-form">
+    <v-text-field clearable label="Name" variant="solo-filled" class="input" v-model="name" />
+    <v-text-field clearable label="LastName" variant="solo-filled" class="input" v-model="name" />
+    <v-text-field clearable label="Number" variant="solo-filled" class="input" v-model="name" />
+    <div class="btns">
+      <v-btn class="btn" color="#17a589" @click="submitForm()">submit</v-btn>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name:'',
+      lastName:'',
+      number:''
+    }
+  },
+}
+</script>
+
+<style>
+.main-form{
+  padding: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.input{
+  width: 500px;
+}
+
+.btns{
+  width: 500px;
+  display: flex;
+  justify-content: right;
+}
+
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,31 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/*//Pinia
+import { createPinia } from 'pinia'
+const pinia = createPinia()
+
+//Vue-Router
+import router from './router/router.js'*/
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
+createApp(App).use(vuetify).mount('#app')
+//.use(router).use(pinia)
