774c11b26259ae5c5e1e5fed4fa5959cde709bb4
lonshakova <lonshakova.ae@edu.spbstu.ru>
2024-08-22 13:46:16 +0300

    123

--- src/components/MainForm.vue
+++ src/components/MainForm.vue
@@ -1,5 +1,19 @@
 <template>
-  <form class="main-form">
+  <div class="header">
+    <v-btn
+      @click="isForm = !isForm"
+      class="btn"
+      >Form</v-btn
+    >
+    <v-btn
+      @click="isUsers = !isUsers"
+      class="btn"
+      >Users</v-btn
+    >
+  </div>
+  <form
+    class="main-form"
+    v-if="isForm">
     <v-text-field
       clearable
       label="Name"
@@ -17,39 +31,79 @@
       label="Number"
       variant="solo-filled"
       class="input"
-      :counter="7"
       v-model="user.number" />
     <div class="btns">
       <v-btn
-        class="btn"
         color="#17a589"
         @click="submitForm()"
         >submit</v-btn
       >
     </div>
   </form>
+  <div
+    class="data-users"
+    v-for="user in users"
+    v-if="isUsers">
+    <div class="user">
+      <div>Name: {{ user.name }}</div>
+      <div>LastName: {{ user.lastName }}</div>
+      <div>Number: {{ user.number }}</div>
+      <div class="user-buton">
+        <v-btn
+          color="#663399"
+          @click="deleteUser(user)"
+          >delete</v-btn
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     data() {
       return {
+        isForm: true,
+        isUsers: true,
         user: {
+          id: "",
           name: "",
           lastName: "",
           number: "",
         },
+        users: [],
       };
     },
     methods: {
       submitForm() {
-        alert(JSON.stringify(this.user));
+        this.user.id = Date.now();
+        this.users.push(this.user);
+        this.user = {
+          id: "",
+          name: "",
+          lastName: "",
+          number: "",
+        };
+      },
+      deleteUser(user) {
+        this.users = this.users.filter((p) => p.id !== user.id);
       },
     },
   };
 </script>
 
 <style>
+  .header {
+    padding: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .btn {
+    margin: 0px 70px;
+  }
+
   .main-form {
     padding: 25px;
     display: flex;
@@ -66,5 +120,26 @@
     width: 500px;
     display: flex;
     justify-content: right;
+  }
+
+  .data-users {
+    padding: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  .user {
+    padding: 10px;
+    width: 500px;
+    font-size: 25px;
+    border: 10px #663399 double;
+    border-radius: 20px;
+  }
+
+  .user-buton{
+    display: flex;
+    justify-content: end;
   }
 </style>
